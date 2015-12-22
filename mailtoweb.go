5369f19722ebf624b06b949d341e4d52231747f9
Arthur White <arthur@whitedevops.com>
2015-12-22 13:09:10 +0100

    Update GoDoc

--- mailtoweb.go
+++ mailtoweb.go
@@ -1,6 +1,28 @@
-// Package mailtoweb returns the webmail URL related to a an email address.
-//
-// It can be used in web apps for a better user experience, by providing a direct link to user's webmail from his email address, after a subscription for example.
+/*
+Package mailtoweb returns the webmail URL related to a an email address.
+
+It can be used in web apps for a better user experience, by providing a direct link to user's webmail from his email address, after a subscription for example.
+
+Installation
+
+In the terminal:
+
+	$ go get github.com/whitedevops/mailtoweb
+
+Usage
+
+Example:
+
+	package main
+
+	import (
+		"github.com/whitedevops/mailtoweb"
+	)
+
+	func main() {
+		mailtoweb.For("user@gmail.com")
+	}
+*/
 package mailtoweb
 
 import "strings"
